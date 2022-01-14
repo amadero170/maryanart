317bd215a1ab55c16a58c4f7c95ab8d6143e8cdd
amadero170 <84208214+amadero170@users.noreply.github.com>
2022-01-14 17:41:39 -0600

    Second commit

--- src/ImagenesFondo.js
+++ src/ImagenesFondo.js
@@ -4,6 +4,10 @@ import foto2 from './imagenes/foto2.jpg'
 import foto3 from './imagenes/foto3.jpg'
 import foto4 from './imagenes/foto4.jpg'
 import foto5 from './imagenes/foto5.jpg'
+import foto6 from './imagenes/foto6.jpg'
+import foto7 from './imagenes/foto7.jpg'
+
+
 import './ImagenesFondo.css'
 import {useState, useEffect} from 'react';
 import {AiOutlineInstagram} from 'react-icons/ai'
@@ -13,21 +17,23 @@ import {AiOutlineInstagram} from 'react-icons/ai'
 export default function ImagenesFondo() {
 const [count, setCount] = useState(0);
 
+const arr = [foto1,foto2,foto3,foto4,foto5,foto6,foto7]
+
   useEffect(() => {
     setInterval(() => {
-      setCount(prevCount => prevCount=== 4 ? prevCount=0 : prevCount+ 1);
+      setCount(prevCount => prevCount=== arr.length-1 ? prevCount=0 : prevCount+ 1);
     }, 4000);
-  }, []);
+  },[]);
 
-  const arr = [foto1,foto2,foto3,foto4,foto5]
+
     return (
         <div className='fotoContainer'>
             <img className='fotoFondo' src={arr[count]} alt='foto'/>
             <div className="titulo">
-              <h1>MURALES | DISEÑO</h1>
+              <h1>MURALS | DESIGN</h1>
               <h2>MARIANA VALDIVIA REYES</h2>
             </div>
-            <a className='sup' href=''>QIUBO</a>
+            <a className='sup' href=''>ENTER</a>
             <a href="https://www.instagram.com/maryanart_design/?hl=en" target="_blank" rel="noopener noreferrer">
             <AiOutlineInstagram className='ig'/>
             </a>
